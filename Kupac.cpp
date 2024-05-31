ccb77d9aefe3920da8905ac099f8aecac0aeca05
UrosMet <wproguros@gmail.com>
2024-05-31 19:59:57 +0200

    Update Kupac

--- Kupac.cpp
+++ Kupac.cpp
@@ -1,7 +1,52 @@
 #include "Kupac.h"
-#include<string>
-#include<iostream>
-void Kupac::info()
-{
-	std::cout << "Ime kupca: " << this->ime << "Prezime kupca: " << this->prezime << "Godine kupca: " << this->godine << "Broj telefona: " << this->brojTelefona << std::endl;
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Kupac::Kupac() {
+    this->godine = 0;
+    this->brojTelefona = "";
 }
+
+Kupac::Kupac(string ime, string prezime, int godine, string brojTelefona)
+    : Osoba(ime, prezime), godine(godine), brojTelefona(brojTelefona) {}
+
+void Kupac::setIme(string ime) {
+    this->ime = ime;
+}
+
+string Kupac::getIme() const {
+    return this->ime;
+}
+
+void Kupac::setPrezime(string prezime) {
+    this->prezime = prezime;
+}
+
+string Kupac::getPrezime() const {
+    return this->prezime;
+}
+
+void Kupac::setGodine(int godine) {
+    this->godine = godine;
+}
+
+int Kupac::getGodine() const {
+    return this->godine;
+}
+
+void Kupac::setBrojTelefona(string brojTelefona) {
+    this->brojTelefona = brojTelefona;
+}
+
+string Kupac::getBrojTelefona() const {
+    return this->brojTelefona;
+}
+
+void Kupac::info() {
+    cout << "Ime kupca: " << this->ime << ", Prezime kupca: " << this->prezime
+        << ", Godine kupca: " << this->godine << ", Broj telefona: " << this->brojTelefona << endl;
+}
+
+Kupac::~Kupac() {}
--- Kupac.h
+++ Kupac.h
@@ -1,48 +1,30 @@
 #pragma once
-#include"Osoba.h"
-#include<string>
-class Kupac : public Osoba
-{
+#include "Osoba.h"
+#include <string>
+
+using namespace std;
+
+class Kupac : public Osoba {
 private:
-	int godine;
-	std::string brojTelefona;
+    int godine;
+    string brojTelefona;
+
 public:
-	Kupac() {
-		this->godine = 0;
-		this->brojTelefona = "";
-	}
-	Kupac(std::string ime, std::string prezime, int godine, std::string brojTelefona) : Osoba(ime, prezime), godine(godine), brojTelefona(brojTelefona) {}
+    Kupac();
+    Kupac(string ime, string prezime, int godine, string brojTelefona);
 
-	void setIme(std::string ime) {
-		this->ime = ime;
-	}
-	std::string getIme() {
-		return this->ime = ime;
-	}
+    void setIme(string ime);
+    string getIme() const;
 
-	void setPrezime(std::string prezime) {
-		this->prezime = prezime;
-	}
-	std::string getPrezime() {
-		return this->prezime = prezime;
-	}
+    void setPrezime(string prezime);
+    string getPrezime() const;
 
-	void setGodine(int godine) {
-		this->godine = godine;
-	}
-	int getGodine() {
-		return this->godine = godine;
-	}
+    void setGodine(int godine);
+    int getGodine() const;
 
-	void setBrojTelefona(std::string brojTelefona) {
-		this->brojTelefona = brojTelefona;
-	}
-	std::string getBrojTelefona() {
-		return this->brojTelefona = brojTelefona;
-	}
+    void setBrojTelefona(string brojTelefona);
+    string getBrojTelefona() const;
 
-
-	void info();
-	~Kupac() {};
+    void info();
+    ~Kupac();
 };
-
